5dc833f0d9bf537a13642c35f6cb046cab1b5f45
Valery Bugakov <skymk1@gmail.com>
2019-08-13 23:35:13 +0300

    Removed eslint

--- webpack.config.js
+++ webpack.config.js
@@ -9,24 +9,24 @@ module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.ts'),
   output: {
     path: path.resolve(__dirname, 'lib'),
-    filename: 'react-selectable-fast.js',
+    filename: 'react-selectable-fast.js'
   },
   module: {
     rules: [
       {
         test: /\.(js|ts|tsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
+        loader: 'babel-loader'
+      }
+    ]
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js']
   },
   plugins: [
     // new BundleAnalyzerPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(env),
-    }),
-  ],
+      'process.env.NODE_ENV': JSON.stringify(env)
+    })
+  ]
 }
