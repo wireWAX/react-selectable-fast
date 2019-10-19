8b1d1aa46f80361e9f5d1d7971f0f6bd1ddfdf99
jfulse <jorgen.fulsebakke@gmail.com>
2019-10-19 16:21:47 +0200

    Add options selectOnClick  and allowShiftClick

--- src/utils/detectMouseButton.ts
+++ src/utils/detectMouseButton.ts
@@ -1,13 +1,21 @@
 import { MouseEvent } from 'react'
 
+type TDetectMouseButtonOptions = {
+  allowShiftClick?: Boolean
+}
+
 /**
  * @buttonNumber
  * 1: Left button
  * 2: Middle/Right button
  * 3: Right/Back button
  */
-export function detectMouseButton(evt: MouseEvent<HTMLElement>, buttonNumber = 1) {
-  if (evt.metaKey || evt.ctrlKey || evt.altKey || evt.shiftKey) {
+export function detectMouseButton(
+  evt: MouseEvent<HTMLElement>,
+  buttonNumber = 1,
+  options: TDetectMouseButtonOptions = {}
+) {
+  if (evt.metaKey || evt.ctrlKey || evt.altKey || (evt.shiftKey && !options.allowShiftClick)) {
     return false
   }
 
